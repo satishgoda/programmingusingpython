b01985c10e85ab0af4a4d2d124576a484c041913
Satish Goda <satishgoda@gmail.com>
2014-03-03 15:55:42 +0530

    Create outputfilenames.py

---
+++ sandbox/inheritance/outputfilenames.py
@@ -0,0 +1,40 @@
+import os
+
+class OutputFilename(object):
+    filename = 'untitled'
+    ext = 'blend'
+    suffix = None
+    suffix_char = '_'
+    dirname = os.getcwd()
+    
+    def __init__(self, context):
+        import bpy
+        import os
+        _filepath = context.blend_data.filepath
+        self.filename = bpy.path.display_name_from_filepath(_filepath)
+        self.dirname = os.path.dirname(_filepath)
+    
+    @property
+    def filepath(self):
+        import os
+        filename = self.filename
+        if self.suffix:
+            filename += self.suffix_char
+            filename += self.suffix
+        filename += '.' + self.ext
+        return os.path.join(self.dirname, filename)
+
+
+class OutputImageFilename(OutputFilename):
+    ext = 'png'
+    def __init__(self, context):
+        super(OutputImageFilename, self).__init__(context)    
+    
+        
+if __name__ == '__main__':
+    import bpy
+    context = bpy.context
+    ofn1 = OutputFilename(context)
+    print(ofn1.filepath)
+    oifn1 = OutputImageFilename(context)
+    print(oifn1.filepath)

8bc9a3b4b7938e190031b6a104a0bb94a2a2399d
Satish Goda <satishgoda@gmail.com>
2014-03-03 19:17:57 +0530

    Update outputfilenames.py

--- sandbox/inheritance/outputfilenames.py
+++ sandbox/inheritance/outputfilenames.py
@@ -13,14 +13,16 @@ class OutputFilename(object):
         _filepath = context.blend_data.filepath
         self.filename = bpy.path.display_name_from_filepath(_filepath)
         self.dirname = os.path.dirname(_filepath)
+
+    def getSuffix(self):
+        return self.suffix_char + self.suffix
     
     @property
     def filepath(self):
         import os
         filename = self.filename
         if self.suffix:
-            filename += self.suffix_char
-            filename += self.suffix
+            filename += self.getSuffix()
         filename += '.' + self.ext
         return os.path.join(self.dirname, filename)
 

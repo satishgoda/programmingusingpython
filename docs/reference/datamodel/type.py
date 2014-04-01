e378a4755ad725cd877e40723f73c5dfb6d14517
Satish Goda <satishgoda@gmail.com>
2014-04-01 15:00:54 +0530

    Update type.py
    <class '__main__.MyMeta'>.__new__(...)  creating A
            bases: ()
            classdict: {'__qualname__': 'A', '__doc__': 'My custom meta class', '__module__': '__main__'}
            args: {}
            Customizing
                    bases: (<class '__main__.aobject'>,)
            Returned
                    <class '__main__.A'>
    <class '__main__.A'>
    <__main__.A object at 0x2aaaba896c50>
    <class '__main__.MyMeta'>
    <class '__main__.A'>
    application base object

--- docs/reference/datamodel/type.py
+++ docs/reference/datamodel/type.py
@@ -1,10 +1,24 @@
+from itertools import chain
+
+
+class aobject:
+    whatsit = 'application base object'
 
 
 class MyMeta(type):
     def __new__(cls, name, bases, classdict, **args):
         _args = (cls, name, bases, classdict, args)
-        print("{}.__new__ called for creating {}\n\tbases: {}\n\tclassdict: {}\n\targs: {}".format(*_args))
+        _args_str = "{}.__new__(...)  creating {}\n\tbases: {}\n\tclassdict: {}\n\targs: {}"
+        print(_args_str.format(*_args))
+        
+        bases = tuple(chain.from_iterable((bases, (aobject,))))
+        
+        print("\tCustomizing\n\t\tbases: {}".format(bases))
+        
         result = type.__new__(cls, name, bases, classdict)
+        
+        print("\tReturned\n\t\t{}".format(result))
+        
         return result
 
 
@@ -19,3 +33,5 @@ if __name__ == '__main__':
     print(a)
     print(type(A))
     print(type(a))
+    print(a.whatsit)
+

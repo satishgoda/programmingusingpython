e1abe2cf4a8806af77a87577aca5df4f525ca3ed
Satish Goda <satishgoda@gmail.com>
2014-04-07 14:40:52 +0530

    Update meta.py
    Created class object <class '__main__.AppType1'> using metaclass <class '__main__.AppMeta'>
    Initializing class object <class '__main__.AppType1'>
    Created instance object <__main__.AppType1 object at 0x2aaab2750450> using class <class '__main__.AppType1'>
    Initializing instance object <__main__.AppType1 object at 0x2aaab2750450>
    Created instance object <__main__.AppType1 object at 0x2aaab2750490> using class <class '__main__.AppType1'>
    Initializing instance object <__main__.AppType1 object at 0x2aaab2750490>
    {}
    {}

--- docs/reference/datamodel/meta.py
+++ docs/reference/datamodel/meta.py
@@ -4,21 +4,26 @@ class AppMeta(type):
         class_object = type.__new__(cls, name, bases, classdict)
         print("Created class object {} using metaclass {}".format(class_object, cls))
         return class_object
-    
+
     def __init__(cls, name, bases, classdict, *args):
         print("Initializing class object {}".format(cls))
 
-        
+
 class AppType1(metaclass=AppMeta):
-    def __new__(cls, *args, **kwargs):
-        instance_object = super(AppType1, cls).__new__(cls, *args, **kwargs)
+    def __new__(cls, **kwargs):
+        instance_object = super(AppType1, cls).__new__(cls)
         print("Created instance object {} using class {}".format(instance_object, cls))
         return instance_object
     
-    def __init__(self):
+    def __init__(self, **kwargs):
         print("Initializing instance object {}".format(self))
 
 
 if __name__ == '__main__':
-    ati1 = AppType1()
+    ati1 = AppType1(**{'f':"f", 'ff':"ff"})
     ati2 = AppType1()
+    
+    from pprint import pprint as print
+    
+    print(vars(ati1))
+    print(vars(ati2))

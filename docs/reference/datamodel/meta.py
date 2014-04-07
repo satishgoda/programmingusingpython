aff325fa8364ba3d1a83e77bf1d9443bfb276f72
Satish Goda <satishgoda@gmail.com>
2014-04-07 16:17:46 +0530

    Update meta.py
    Created class object <class '__main__.AppType1'> using metaclass <class '__main__.AppMeta'>
    Initializing class object <class '__main__.AppType1'>
    Created instance object <__main__.AppType1 object at 0x2aaab273b050> using class <class '__main__.AppType1'>
    Initializing instance object <__main__.AppType1 object at 0x2aaab273b050>
    Created instance object <__main__.AppType1 object at 0x2aaab273b0d0> using class <class '__main__.AppType1'>
    Initializing instance object <__main__.AppType1 object at 0x2aaab273b0d0>
    Initializing instance object <__main__.AppType1 object at 0x2aaab273b050>
    {'id': 1, 'type': 'Mesh'}
    {'id': 2, 'type': 'Mesh'}
    {'id': 1, 'type': 'Mesh'}

--- docs/reference/datamodel/meta.py
+++ docs/reference/datamodel/meta.py
@@ -10,20 +10,33 @@ class AppMeta(type):
 
 
 class AppType1(metaclass=AppMeta):
+    _instances = dict()
     def __new__(cls, **kwargs):
-        instance_object = super(AppType1, cls).__new__(cls)
-        print("Created instance object {} using class {}".format(instance_object, cls))
+        oid = kwargs['id']
+        otype = kwargs['type']
+        name = "{}.{}".format(otype, oid)
+        if name in cls._instances:
+            instance_object = cls._instances[name]
+        else:
+            instance_object = super(AppType1, cls).__new__(cls)
+            cls._instances[name] = instance_object
+            print("Created instance object {} using class {}".format(instance_object, cls))
         return instance_object
     
     def __init__(self, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
         print("Initializing instance object {}".format(self))
 
 
+
 if __name__ == '__main__':
-    ati1 = AppType1(**{'f':"f", 'ff':"ff"})
-    ati2 = AppType1()
+    ati1 = AppType1(**{'id':1, 'type': 'Mesh'})
+    ati2 = AppType1(**{'id':2, 'type': 'Mesh'})
+    ati3 = AppType1(**{'id':1, 'type': 'Mesh'})
     
     from pprint import pprint as print
     
     print(vars(ati1))
     print(vars(ati2))
+    print(vars(ati3))

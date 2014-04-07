bf2114b8dd3f5231680b96c78abe7a62ecee1836
Satish Goda <satishgoda@gmail.com>
2014-04-07 11:06:56 +0530

    Update meta.py
    Created class object <class '__main__.AppType1'> using metaclass <class '__main__.AppMeta'>
    Initializing class object <class '__main__.AppType1'>
    Created instance object <__main__.AppType1 object at 0x2aaab2734d10> using class <class '__main__.AppType1'>
    Initializing instance object <__main__.AppType1 object at 0x2aaab2734d10>
    Created instance object <__main__.AppType1 object at 0x2aaab27343d0> using class <class '__main__.AppType1'>
    Initializing instance object <__main__.AppType1 object at 0x2aaab27343d0>

--- docs/reference/datamodel/meta.py
+++ docs/reference/datamodel/meta.py
@@ -1,5 +1,4 @@
 
-
 class AppMeta(type):
     def __new__(cls, name, bases, classdict, *args):
         class_object = type.__new__(cls, name, bases, classdict)
@@ -9,11 +8,17 @@ class AppMeta(type):
     def __init__(cls, name, bases, classdict, *args):
         print("Initializing class object {}".format(cls))
 
-
+        
 class AppType1(metaclass=AppMeta):
+    def __new__(cls, *args, **kwargs):
+        instance_object = super(AppType1, cls).__new__(cls, *args, **kwargs)
+        print("Created instance object {} using class {}".format(instance_object, cls))
+        return instance_object
+    
     def __init__(self):
         print("Initializing instance object {}".format(self))
 
 
 if __name__ == '__main__':
     ati1 = AppType1()
+    ati2 = AppType1()

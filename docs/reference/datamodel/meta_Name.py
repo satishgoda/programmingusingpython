5de03d84369cb2191c98736dd53255cf7266a38b
Satish Goda <satishgoda@gmail.com>
2014-04-08 14:27:33 +0530

    Update meta_Name.py

--- docs/reference/datamodel/meta_Name.py
+++ docs/reference/datamodel/meta_Name.py
@@ -1,7 +1,16 @@
 
 
-class Name():
-    Name = "Name is Name"
+class AddClassName(type):
+    def __new__(cls, name, bases, classdict):
+        class_object = type.__new__(cls, name, bases, classdict)
+        return class_object
+
+    def __init__(cls, name, bases, classdict):
+        setattr(cls, cls.__name__, cls.__name__)
+
+
+class Name(metaclass=AddClassName):
+    pass
 
 
 if __name__ == '__main__':

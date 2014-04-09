b2c3fa52230b87efdab67d5c606ef87ee65cb9ac
Satish Goda <satishgoda@gmail.com>
2014-04-09 18:42:24 +0530

    Update classtree.py
    [(<type 'object'>, ()),
     [(<class 'turtle.TNavigator'>, (<type 'object'>,)),
      (<class 'turtle.TPen'>, (<type 'object'>,)),
      [(<class 'turtle.RawTurtle'>,
        (<class 'turtle.TPen'>, <class 'turtle.TNavigator'>)),
       [(<class 'turtle.Turtle'>, (<class 'turtle.RawTurtle'>,))]]]]
    object()
    TNavigator(object)
    TPen(object)
    RawTurtle(TPen, TNavigator)
    Turtle(RawTurtle)

--- docs/library/inspect/classtree.py
+++ docs/library/inspect/classtree.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
-
 from inspect import getmro, getclasstree
 from pprint import pprint
 import turtle
 
-mro = getmro(turtle.Turtle)
 
-tree = getclasstree(mro, unique=1)
+def trace_bases(iterable):
+    cls_name = iterable[0].__name__
+    bases = []
+    for base in iterable[1]:
+        bases.append(base.__name__)
+    bases_str = ', '.join(bases) if bases else ''
+    print("{0}({1})".format(cls_name, bases_str))
 
-pprint(tree)
+
+def trace_tree(iterable):
+    for item in iterable:
+        if isinstance(item, tuple):
+            trace_bases(item)
+        else:
+            trace_tree(item)
+
+
+if __name__ == '__main__':
+    mro = getmro(turtle.Turtle)
+    
+    tree = getclasstree(mro, unique=1)
+    
+    pprint(tree)
+    
+    trace_tree(tree)

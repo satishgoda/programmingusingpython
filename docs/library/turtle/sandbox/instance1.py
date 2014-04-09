6abf50383f85e5dd0bcae33f33414c3731fbb44f
Satish Goda <satishgoda@gmail.com>
2014-04-09 17:16:03 +0530

    Create instance1.py
    canvas = turtle.Turtle()

---
+++ docs/library/turtle/sandbox/instance1.py
@@ -0,0 +1,22 @@
+import turtle
+turtle.Turtle
+turtle.Turtle()
+help(turtle.Turtle)
+locals()
+canvas = turtle.Turtle()
+canvas.screen
+canvas.screen()
+canvas.screen.setup(600, 600)
+canvas.penup()
+canvas.forward(100)
+canvas.pendown()
+canvas.backward(100)
+canvas.clear()
+screen
+canvas.screen
+canvas.screen.bye()
+canvas
+canvas.showturtle()
+canvas.screen
+canvas.screen.setup()
+del canvas

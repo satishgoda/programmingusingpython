755870a45b3387be749bf2378bfac481adae2e63
Satish Goda <satishgoda@gmail.com>
2014-04-10 11:11:13 +0530

    Update classtree.py
    [(<type 'object'>, ()),                                                                                              
     [(<type 'exceptions.BaseException'>, (<type 'object'>,)),                                                           
      [(<type 'exceptions.Exception'>, (<type 'exceptions.BaseException'>,)),                                            
       [(<type 'exceptions.StandardError'>, (<type 'exceptions.Exception'>,)),                                           
        [(<type 'exceptions.EnvironmentError'>,                                                                          
          (<type 'exceptions.StandardError'>,)),                                                                         
         [(<type 'exceptions.OSError'>,                                                                                  
           (<type 'exceptions.EnvironmentError'>,))]]]]]]                                                                
    
    object()
    BaseException(object)
    Exception(BaseException)
    StandardError(Exception)
    EnvironmentError(StandardError)
    OSError(EnvironmentError)

--- docs/library/inspect/classtree.py
+++ docs/library/inspect/classtree.py
@@ -2,28 +2,32 @@
 
 from inspect import getmro, getclasstree
 from pprint import pprint
-import turtle
 
 
-def trace_bases(iterable):
-    cls_name = iterable[0].__name__
-    bases =[base.__name__ for base in iterable[1]]
-    bases_str = ', '.join(bases) if bases else ''
-    print("{0}({1})".format(cls_name, bases_str))
-
-
-def trace_tree(iterable):
-    for item in iterable:
-        trace = trace_bases if isinstance(item, tuple) else trace_tree
-        trace(item)
-
-
-if __name__ == '__main__':
-    import exceptions
-    mro = getmro(exceptions.OSError)
+def trace(this):
+    def _trace_tree(iterable):
+        def _trace_bases(iterable):
+            cls_name = iterable[0].__name__
+            bases =[base.__name__ for base in iterable[1]]
+            bases_str = ', '.join(bases) if bases else ''
+            print("{0}({1})".format(cls_name, bases_str))
+            
+        for item in iterable:
+            trace = _trace_bases if isinstance(item, tuple) else _trace_tree
+            trace(item)
+    
+    mro = getmro(this)
     
     tree = getclasstree(mro, unique=1)
     
     pprint(tree)
     
-    trace_tree(tree)
+    print('')
+    
+    _trace_tree(tree)
+
+
+if __name__ == '__main__':
+    import exceptions
+    
+    trace(exceptions.OSError)

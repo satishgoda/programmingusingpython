f65139ad93ffea89adccf6051b73e9494d324a27
Satish Goda <satishgoda@gmail.com>
2014-04-11 17:06:43 +0530

    Update prog.py
    >>> prog.py
    usage: prog.py [-h] N [N ...]
    prog.py: error: too few arguments
    
    
    >>> prog.py --help
    usage: prog.py [-h] N [N ...]
    
    Process some integers.
    
    positional arguments:
      N           a sequence of integers for the program to process
    
    optional arguments:
      -h, --help  show this help message and exit
    
    
    >>> prog.py 1 2 3
    [1, 2, 3]

--- docs/library/argparse/prog.py
+++ docs/library/argparse/prog.py
@@ -4,6 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Process some integers.")
 
+# positional argument
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='a sequence of integers for the program to process'
+                    )
+
 args = parser.parse_args()
 
-print(args)
+print(args.integers)

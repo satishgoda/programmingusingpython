0bde5e6e708bb1e5d72d143208c8e64ecab22b46
Satish Goda <satishgoda@gmail.com>
2014-04-16 11:02:02 +0530

    Update bapp.py

--- docs/library/subprocess/bapp.py
+++ docs/library/subprocess/bapp.py
@@ -2,20 +2,37 @@
 
 from subprocess  import Popen
 import os
+import itertools
+
+class RequiredBlenderStartupError(Exception):
+    pass
 
 
-p = Popen("blender", env={
-                   'BLENDER_USER_CONFIG': './config', 
-                   'BLENDER_USER_SCRIPTS' : './scripts', 
-                   'DISPLAY': ':0',
-                   'PYTHONPATH': '',
-                   'PATH': '/home/satishg/bin/blenderinstalldir/'
-               })
+_required_directories = ('./config', './scripts')
 
 
-print("Parent {0} launched child {1}".format(os.getpid(), p.pid))
+try:
+    if not all(itertools.imap(os.path.exists, _required_directories)):
+        raise RequiredBlenderStartupError("Mwhahahha")
+except RequiredBlenderStartupError as e:
+    print(e)
+    import sys
+    sys.exit(1)
+else:
+    p = Popen("blender", env={
+                    'BLENDER_USER_CONFIG': _required_directories[0], 
+                    'BLENDER_USER_SCRIPTS' : _required_directories[1], 
+                    'DISPLAY': ':0',
+                    'PYTHONPATH': '',
+                    'PATH': '/home/satishg/bin/blenderinstalldir/'
+                })
 
-retcode = p.wait()
 
-print("Child process terminited with retcode {0}".format(retcode))
+    print("Parent {0} launched child {1}".format(os.getpid(), p.pid))
+
+    retcode = p.wait()
+
+    print("Child process {0} terminited with retcode {1}".format(p.pid, retcode))
+finally:
+    print "Bye bye"
 

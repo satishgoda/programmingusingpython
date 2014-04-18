f44a79afd8e2380f830a496d576b1aecbbc8c002
Satish Goda <satishgoda@gmail.com>
2014-04-18 11:51:26 +0530

    Create notamodule.py
    linux:~> python
    Python 2.6.6 (r266:84292, Feb 22 2013, 00:00:18)
    [GCC 4.4.7 20120313 (Red Hat 4.4.7-3)] on linux2
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import notamodule
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "notamodule.py", line 3, in <module>
        assert __name__ == '__main__', "You cannot import this as a module"
    AssertionError: You cannot import this as a module
    >>>
    
    
    linux:~> python notamodule.py
    Good. This is how this script was intended to be used

---
+++ docs/reference/lexical/keywords/assert/notamodule.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+assert __name__ == '__main__', "You cannot import this as a module"
+
+if __name__ == '__main__':
+    print("Good. This is how this script was intended to be used")
+

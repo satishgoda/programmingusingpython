a845c2354ff16887fe82addbba23c47b40726536
Satish Goda <satishgoda@gmail.com>
2014-04-18 16:18:12 +0530

    Update os_kill_signal.py

--- docs/library/signal/os_kill_signal.py
+++ docs/library/signal/os_kill_signal.py
@@ -6,3 +6,12 @@ pid = ......
 
 
 os.kill(pid, signal.SIGINT)
+
+# Freezes aspp
+os.kill(2152, signal.SIGSTOP)
+
+# Unfreezes app
+os.kill(2152, signal.SIGCONT)
+
+# kill -9
+os.kill(2152, signal.SIGKILL)

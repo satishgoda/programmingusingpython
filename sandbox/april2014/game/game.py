2d6f2f1bdc4b490ad2a159576e4d0c9d36bd1e04
Satish Goda <satishgoda@gmail.com>
2014-04-21 16:42:48 +0530

    Update game.py
    Test # Help | Quit
    -------------------------------------------------------------------------------
    Welcome to this application
    -------------------------------------------------------------------------------
    user says >>> quit
    Quitting application Test

--- sandbox/april2014/game/game.py
+++ sandbox/april2014/game/game.py
@@ -9,26 +9,54 @@ assert __name__ == '__main__', "One does not simply import a game. One needs to 
 input = raw_input
 
 
+class ApplicationMode(object):
+    def __init__(self, label, *args):
+        app.modes['list'][label] = self
+
+
+class ApplicationHelpMode(ApplicationMode):
+    def __init__(self, *args):
+        super(ApplicationHelpMode, self).__init__("Help", *args)
+
+    def view(self):
+        print("This is how you use the application")
+
+    def user_interaction(self):
+        pass
+
+
 class Application(object):
     def __init__(self, name):
         self.name = name
-        self.modes = None
-        self.actions = {}
-        self.actions['quit'] = self.quit
 
-    def quit(self):
+        self.modes = {}
+        self.modes['previous'] = None
+        self.modes['current'] = 'app'
+        self.modes['list'] = {}
+
+        self.actions = {}
+        self.actions['Quit'] = self.action_quit
+
+    def action_quit(self):
         print("{0} application {1}".format("Quitting", self.name))
         sys.exit(0)
 
     def menu(self):
-        menuText = ["{0} | ".format(self.name)]
-        if not self.modes:
-            menuText.append('{0}'.format("Quit"))
+        menuText = ["{0} # ".format(self.name)]
+
+        if self.modes['current'] == 'app':
+            modes = self.modes['list']
+            others = ' | '.join(modes.keys()+['Quit'])
+            menuText.append(others)
+
         print(''.join(menuText))
 
     def user_interaction(self):
-        if not self.modes:
-            command = input("user says >>> ").strip()
+        if self.modes['current'] == 'app':
+            try:
+                command = input("user says >>> ").strip().capitalize()
+            except EOFError as e:
+                return
             if not command: return
 
             if command not in self.actions:
@@ -37,7 +65,8 @@ class Application(object):
                 self.actions[command]()
 
     def view(self):
-        print("Welcome to this application")
+        if self.modes['current'] == 'app':
+            print("Welcome to this application")
 
     def execute(self):
         os.system('clear')
@@ -54,8 +83,11 @@ class Application(object):
         raise KeyboardInterrupt(self)
 
 
+
 if __name__ == '__main__':
     app = Application("Test")
+
+    ApplicationHelpMode(app)
 
     signal.signal(signal.SIGINT, app.handle_signal_SIGINT)
 

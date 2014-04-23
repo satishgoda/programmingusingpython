ec4bbf1bab5ca59fbd4619ffa80f6769b35ee50a
Satish Goda <satishgoda@gmail.com>
2014-04-23 14:10:28 +0530

    Create ipython_session.py
    In [1]: %save?
    
        Save a set of lines to a given filename.
    
        Usage:
          %save [options] filename n1-n2 n3-n4 ... n5 .. n6 ...
    
        Options:
    
          -r: use 'raw' input.  By default, the 'processed' history is used,
          so that magics are loaded in their transformed version to valid
          Python.  If this option is given, the raw input as typed as the
          command line is used instead.
    
        This function uses the same syntax as %macro for line extraction, but
        instead of creating a macro it saves the resulting string to the
        filename you specify.
    
        It adds a '.py' extension to the file if you don't do so yourself, and
        it asks for confirmation before overwriting existing files.

---
+++ training/2014/april/2014_04_23/ipython_session.py
@@ -0,0 +1,40 @@
+
+#?argparse?
+#?argparse
+#?argparse.ArgumentParser
+
+
+
+
+_ip.magic("run cmdlineargs.py sdjhfskjf sdkfjhsdkf 2432843")
+_ip.magic("run cmdlineargs.py")
+_ip.magic("run cmdlineargs.py")
+_ip.magic("run cmdlineargs.py --help")
+_ip.magic("run cmdlineargs.py --help")
+_ip.magic("run cmdlineargs.py --help")
+_ip.magic("run cmdlineargs.py --help")
+_ip.magic("run cmdlineargs.py")
+_ip.magic("run cmdlineargs.py --debug")
+_ip.magic("run cmdlineargs.py ")
+_ip.magic("run cmdlineargs.py -d")
+_ip.magic("run cmdlineargs.py")
+_ip.magic("run cmdlineargs.py 1 2")
+_ip.magic("run cmdlineargs.py 1 2")
+_ip.magic("run cmdlineargs.py 1.0 2")
+_ip.magic("run cmdlineargs.py 1 2 3")
+_ip.magic("run cmdlineargs.py 1 2")
+
+
+
+_ip.system("date")
+import time
+
+
+time.asctime()
+
+
+
+
+
+#?%save
+_ip.magic("history ")

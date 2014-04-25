cf6b1862ea515a182bf52db82be55a16299f98f5
Satish Goda <satishgoda@gmail.com>
2014-04-25 09:20:43 +0530

    Create whats_HUP.py
    Caught hup at Fri Apr 25 09:05:32 2014
    Caught hup at Fri Apr 25 09:05:48 2014
    Caught hup at Fri Apr 25 09:08:58 2014
    Caught hup at Fri Apr 25 09:08:58 2014
    Caught hup at Fri Apr 25 09:09:42 2014
    Caught hup at Fri Apr 25 09:09:42 2014
    Caught hup at Fri Apr 25 09:14:36 2014
    Caught hup at Fri Apr 25 09:15:13 2014
    Caught hup at Fri Apr 25 09:17:08 2014
    Caught hup at Fri Apr 25 09:17:30 2014

---
+++ docs/library/signal/whats_HUP.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import signal
+import time
+import os
+
+
+def whats_HUP(_signal, frame):
+    with open("/home/satishg/hup.txt", 'a') as f:
+        f.write("Caught hup at {0}\n".format(time.asctime()))
+    
+    signal.signal(signal.SIGHUP, whats_HUP.original)
+
+
+if __name__ == '__main__':
+    whats_HUP.original = signal.getsignal(signal.SIGHUP)
+    
+    signal.signal(signal.SIGHUP, whats_HUP)
+    
+    try:
+        raw_input("send me (pid {0}) a SIGHUP".format(os.getpid()))
+    except EOFError as eof:
+        with open("/home/satishg/hup.txt", 'r') as f:
+            lines = f.readlines()
+            message = lines[1].strip() if lines else "No previous HUP history saved"
+            print('\n{line}'.format(line=))

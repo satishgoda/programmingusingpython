f76141e25eefaab39865eaef48e6dd7bad169da2
Satish Goda <satishgoda@gmail.com>
2014-05-05 11:29:04 +0530

    Update atexit.py
    linux:~> atexit.py
    Python is not only a reptile
    exitfunc1: Python interpreter is exiting..
    
    linux:~> echo $?
    0

--- docs/library/atexit/atexit.py
+++ docs/library/atexit/atexit.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+import atexit
+
+
+def exitfunc1():
+    print("{0}: Python interpreter is exiting..".format(exitfunc1.__name__))
+
+
+atexit.register(exitfunc1)
+
+
 message = "Python is not only a reptile"
 
 print(message)

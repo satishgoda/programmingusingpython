bc2e3406b3c32e6612e5eab54d788528a6d7224b
Satish Goda <satishgoda@gmail.com>
2014-05-05 11:36:37 +0530

    Update atexit.py
    linux:~> atexit.py
    Python is not only a reptile
    exitfunc1: Python interpreter is exiting..
    ('count', 1)
    ('status', 'OK')
    ('message', 'Python is also a language')
    
    linux:~> echo $?
    0

--- docs/library/atexit/atexit.py
+++ docs/library/atexit/atexit.py
@@ -3,15 +3,27 @@
 import atexit
 
 
-def exitfunc1():
+def exitfunc1(context):
     print("{0}: Python interpreter is exiting..".format(exitfunc1.__name__))
+    for key, value in context.items():
+        print(key, value)
+
+context = {}
+
+atexit.register(exitfunc1, context)
 
 
-atexit.register(exitfunc1)
+context['message'] = "Python is not only a reptile"
+
+print(context['message'])
 
 
-message = "Python is not only a reptile"
+def update_context(key, value, context):
+    context[key] = value
 
-print(message)
+update_context('message', "Python is also a language", context)
+update_context('count', 1, context)
+update_context('status', 'OK', context)
+
 
 exit(0)

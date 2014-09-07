95aa7c128ba4076a6d811552d4c6967cac91a24b
Satish Goda <satishgoda@gmail.com>
2014-09-07 10:35:31 +0800

    Added decorator

--- sandbox/decorators/basics/deco1.py
+++ sandbox/decorators/basics/deco1.py
@@ -2,9 +2,17 @@ __author__ = 'satish goda'
 
 from oopath import Path
 
-def main():
-    p = Path(__file__)
-    print("Running {}".format(p.basename))
+def path_header(wrapped):
+    def wrapper(*args, **kwargs):
+        p = Path(__file__)
+        print("Running {}".format(p.basename))
+        wrapped(*args, **kwargs)
+    return wrapper
+
+@path_header
+def main(*args, **kwargs):
+    print "Decorators are nice"
 
 if __name__ == '__main__':
+    print main
     main()

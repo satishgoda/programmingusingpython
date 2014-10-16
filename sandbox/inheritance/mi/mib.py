89daf753cb5040f009dba322c0c95221d56921be
Satish Goda <satishgoda@gmail.com>
2014-10-17 00:35:55 +0800

    Getting deeper into Multiple Inheritance

---
+++ sandbox/inheritance/mi/mib.py
@@ -0,0 +1,38 @@
+"""
+C
+B
+A
+ABC
+{'a': 'A', 'c': 'C', 'b': 'B', 'abc': 'ABC'}
+"""
+class A(object):
+    def __init__(self):
+        super(A, self).__init__()
+        self.a = 'A' #self.__class__.__name__''
+        print self.a
+
+
+class B(object):
+    def __init__(self):
+        super(B, self).__init__()
+        self.b = 'B' #self.__class__.__name__
+        print self.b
+
+
+class C(object):
+    def __init__(self):
+        super(C, self).__init__()
+        self.c = 'C' #self.__class__.__name__
+        print self.c
+
+
+class ABC(A, B, C):
+    def __init__(self):
+        super(ABC, self).__init__()
+        self.abc = self.__class__.__name__
+        print self.abc
+
+
+if __name__ == '__main__':
+    abc = ABC()
+    print(vars(abc))

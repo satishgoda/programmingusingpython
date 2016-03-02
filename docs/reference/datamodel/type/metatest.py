93773996de69e6b121d9481b89bc6b0ae4f69f23
Satish Goda <satishgoda@live.com>
2016-03-03 06:40:48 +0800

    Digging into meta classes with a simple test : Added output

--- docs/reference/datamodel/type/metatest.py
+++ docs/reference/datamodel/type/metatest.py
@@ -38,3 +38,38 @@ print d21.time
 print Base.time
 print Derived1.time
 print Derived2.time
+
+"""
+>>> (executing lines 1 to 33 of "metatest.py")
+About to define class 'Base' via type '<class '__main__.MetaTest'>'
+(<class '__main__.MetaTest'>, 'Base', (<type 'object'>,), {'__module__': '__main__', '__metaclass__': <class '__main__.MetaTest'>})
+About to initialize class 'Base' that was defined via type '<class '__main__.MetaTest'>'
+(<class '__main__.Base'>, 'Base', (<type 'object'>,), {'__module__': '__main__', '__metaclass__': <class '__main__.MetaTest'>, 'time': <property object at 0x0000005A27EF9BD8>})
+<property object at 0x0000005A27EF9BD8>
+
+About to define class 'Derived1' via type '<class '__main__.MetaTest'>'
+(<class '__main__.MetaTest'>, 'Derived1', (<class '__main__.Base'>,), {'__module__': '__main__'})
+About to initialize class 'Derived1' that was defined via type '<class '__main__.MetaTest'>'
+(<class '__main__.Derived1'>, 'Derived1', (<class '__main__.Base'>,), {'__module__': '__main__', 'time': <property object at 0x0000005A27EF9C28>})
+<property object at 0x0000005A27EF9C28>
+
+About to define class 'Derived2' via type '<class '__main__.MetaTest'>'
+(<class '__main__.MetaTest'>, 'Derived2', (<class '__main__.Base'>,), {'__module__': '__main__'})
+About to initialize class 'Derived2' that was defined via type '<class '__main__.MetaTest'>'
+(<class '__main__.Derived2'>, 'Derived2', (<class '__main__.Base'>,), {'__module__': '__main__', 'time': <property object at 0x0000005A27F05E58>})
+<property object at 0x0000005A27F05E58>
+
+<class '__main__.Derived1'>
+<class '__main__.MetaTest'>
+Thu Mar 03 06:34:18 2016
+Thu Mar 03 06:34:18 2016
+
+>>> Base.time
+<property object at 0x0000005A27EF9BD8>
+
+>>> Derived1.time
+<property object at 0x0000005A27EF9C28>
+
+>>> Derived2.time
+<property object at 0x0000005A27F05E58>
+"""

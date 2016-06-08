dc9a1e4f65877be96ea4df1b8fde6c1563b63159
Satish Goda <satishgoda@live.com>
2016-06-09 00:26:25 +0800

    Create nx_proto1.py

---
+++ sandbox/june2016/wrappers/graph/nx_proto1.py
@@ -0,0 +1,75 @@
+import networkx as nx
+
+nxdg = nx.DiGraph()
+nxdg.add_node('a')
+nxdg.add_node('b')
+nxdg.nodes()
+nxdg.nodes(data=True)
+nxdg.node
+
+class AppClass(object):
+    def __init__(self, name):
+        self.name = name
+
+class GraphNodeAttrs(object):
+    def __init__(self, node, graph):
+        self.__dict__['graph'] = graph
+        self.__dict__['node'] = node
+    
+    @property
+    def node(self):
+        return self.__dict__['graph'].node[self.__dict__['node']]
+    
+    def __getattr__(self, attr):
+        return self.node[attr]
+    
+    def __setattr__(self, attr, value):
+        self.node[attr] = value
+    
+class GraphNode(object):
+    def __init__(self, data, graph):
+        self.data = data
+        self.graph = graph
+        self._attrs = GraphNodeAttrs(self, graph)
+    
+    @property
+    def name(self):
+        return self.data.name
+
+    @property
+    def attrs(self):
+        return self._attrs
+
+class Graph(object):
+    def __init__(self):
+        self.graph = nx.DiGraph(name="Yeah")
+    
+    def add_node(self, node):
+        self.graph.add_node(node)
+        node.attrs.label = node.name
+    
+    @property
+    def node(self):
+        return self.graph.node
+
+    @property
+    def nodes(self):
+        return self.graph.nodes(data=False)
+    
+a1 = AppClass('Apple')
+a2 = AppClass('Ball')
+
+dg = Graph()
+
+a1gn = GraphNode(a1, dg)
+a2gn = GraphNode(a2, dg)
+
+dg.add_node(a1gn)
+dg.add_node(a2gn)
+
+a1gn.attrs.style = 'filled, bold'
+a1gn.attrs.fillcolor = 'green'
+
+print dg.node
+
+print dg.nodes

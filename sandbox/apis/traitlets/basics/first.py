2dc3b6060a5b77817b02cdbff5a5cd48ed26fc3a
Satish Goda <satishgoda@live.com>
2016-06-19 21:56:42 +0800

    ipython traitlets module first steps
    http://traitlets.readthedocs.io/en/stable/using_traitlets.html

---
+++ sandbox/apis/traitlets/basics/first.py
@@ -0,0 +1,28 @@
+# http://traitlets.readthedocs.io/en/stable/using_traitlets.html
+
+import getpass
+
+from traitlets import HasTraits, Unicode, default
+from traitlets import observe, Integer
+
+class Identity(HasTraits):
+    username = Unicode()
+    num = Integer(5, help="a number").tag(config=True)
+
+    @default('username')
+    def _username_default(self):
+        return getpass.getuser()
+    
+    @observe('num')
+    def _num_changed(self, change):
+        print("{name} changed from {old} to {new}".format(**change))
+
+i = Identity()
+
+i.username
+
+i.num
+
+i.num = 10
+
+Identity.class_own_traits()

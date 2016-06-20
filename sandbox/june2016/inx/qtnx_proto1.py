7c610ea41e394770dd4d383d66d21426d35ea67d
Satish Goda <satishgoda@live.com>
2016-06-20 08:25:46 +0800

    Parenting QGraphicsSceneItems and different graph layout

--- sandbox/june2016/inx/qtnx_proto1.py
+++ sandbox/june2016/inx/qtnx_proto1.py
@@ -14,6 +14,11 @@ nxdg.add_node('D')
 nxdg.add_node('E')
 nxdg.add_node('F')
 
+nxdg.add_edge('A', 'E')
+nxdg.add_edge('A', 'C')
+nxdg.add_edge('D', 'B')
+nxdg.add_edge('B', 'F')
+
 ## 
 
 scene = QtGui.QGraphicsScene()
@@ -25,18 +30,25 @@ view.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 view.show()
 
 ## 
+#
+# nxdg_l = nx.layout.circular_layout(nxdg, dim=2, scale=50)
+# nxdg_l = nx.layout.fruchterman_reingold_layout(nxdg, dim=2, center=[0,0], scale=100)
 
-nxdg_l = nx.layout.circular_layout(nxdg, dim=2, scale=50)
+nxdg_l = nx.layout.spring_layout(nxdg, dim=2, center=[0,0], scale=100)
 
 for node_l, pos in nxdg_l.iteritems():
     x, y = pos
-    rect = scene.addRect(x, y, 20, 20)
+    rect = scene.addRect(0, 0, 20, 20)
     rect.setToolTip(node_l)
     text = scene.addText(node_l)
     text.setPos(x, y)
+    rect.setParentItem(text)
 
 ## 
-nxdg_l = nx.layout.circular_layout(nxdg, dim=2, scale=100)
+# nxdg_l = nx.layout.circular_layout(nxdg, dim=2, scale=100)
+# nxdg_l = nx.layout.fruchterman_reingold_layout(nxdg, dim=2, center=[0,0], iterations=300, scale=150)
+
+nxdg_l = nx.layout.spring_layout(nxdg, dim=2, center=[0,0], scale=150)
     
 for item in scene.items():
     if isinstance(item, QtGui.QGraphicsTextItem):

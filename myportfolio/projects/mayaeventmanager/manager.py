d29b7fe77ec3fa19328a6868a419174b3f4407d5
Satish Goda <satishgoda@live.com>
2016-10-21 00:40:51 +0800

    solidifying MayaEventManager

--- myportfolio/projects/mayaeventmanager/manager.py
+++ myportfolio/projects/mayaeventmanager/manager.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import pymel.core as pm
 
 class io(object):
@@ -18,14 +19,25 @@ class MayaEventData(object):
         self.event = event
         self.jid = None
         self.observers = []
-    
+
+    def __repr__(self):
+        return "{0} {1} {2}".format(self.__class__, self.event, self.jid)
+ 
     def startJob(self):
+        if self.jid:
+            io.warn("Already running {0}".format(self))
+            return
+        
         self.jid = pm.scriptJob(event=(self.event, self.notify))
         io.warn("Started {0} job: {1}".format(self.event, self.jid))
     
     def killJob(self):
-        pm.scriptJob(kill=self.jid)
-        io.warn("Killed {0} job: {1}".format(self.event, self.jid))
+        try:
+            pm.scriptJob(kill=self.jid)
+            io.warn("Killed {0} job: {1}".format(self.event, self.jid))
+            self.jid = None
+        except Exception:
+            raise
     
     @property
     def callbackName(self):
@@ -40,6 +52,11 @@ class MayaEventData(object):
             raise RuntimeError("{0} must implement {1}".format(observer, self.callbackName))
 
         self.observers.append(observer)
+
+    def removeObserver(self, observer):
+        if not observer in self.observers:
+            raise ValueError("{0} is not registered with {1} event".format(observer, self.event))
+        self.observers.remove(observer)
 
     def notify(self):
         if not self.observers:
@@ -62,21 +79,22 @@ class MayaEventManager(object):
 
     def initializeEvents(self):
         events = self.events
-
         for event in events:
             events[event] = MayaEventData(event)
         self._initialized = True
 
+    def debug(self):
+        print evtmgr.events['SelectionChanged'].observers
+        print evtmgr.events['ChannelBoxLabelSelected'].observers
+
     def startEvents(self):
         assert self._initialized == True, "Event Manager has not been initialized"
-
         events = self.events
         for event in events:
             events[event].startJob()         
 
     def stopEvents(self):
         assert self._initialized == True, "Event Manager has not been initialized"
-        
         events = self.events
         for event in events:
             events[event].killJob()
@@ -90,4 +108,15 @@ class MayaEventManager(object):
             eventTypes = ", ".join(self.events.keys())
             raise ValueError("Unsupported event type. Supported types are \n{0}".format(eventTypes))
         except Exception as e:
-            raise
+            raise 
+
+    def removeEventObserver(self, event, observer):
+        assert self._initialized == True, "Event Manager has not been initialized"
+        
+        try:
+            self.events[event].removeObserver(observer)
+        except KeyError:
+            eventTypes = ", ".join(self.events.keys())
+            raise ValueError("Unsupported event type. Supported types are \n{0}".format(eventTypes))
+        except Exception:
+            raise 

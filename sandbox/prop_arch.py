1f8d5bbfdad025fe23410a28229698540ea14811
Satish Goda <satishgoda@live.com>
2019-12-17 15:21:06 -0800

    Fix for printing group properties properly

--- sandbox/prop_arch.py
+++ sandbox/prop_arch.py
@@ -1,27 +1,32 @@
 import pdb
 
+
 class Property(object):
     def __get__(self, instance, owner):
         if instance is None:
             return self
         else:
             return instance.__dict__.get(self.name, self.default)
-        
+
     def __set__(self, instance, value):
         instance.__dict__[self.name] = value
-        
+
     def __repr__(self):
         s = super(Property, self).__repr__()
         return self.name + " " + s
 
+
 class IntProperty(Property):
     default = 0
-    
+
+
 class FloatProperty(Property):
     default = 0.0
-    
+
+
 class StringProperty(Property):
     default = "You did not specify anything"
+
 
 class PropertiesBased(type):
 
@@ -38,6 +43,7 @@ class PropertiesBased(type):
         cls = super(mcs, mcs).__new__(mcs, name, bases, namespace)
         return cls
 
+
 class PropertyGroup(Property):
     __metaclass__ = PropertiesBased
 
@@ -53,31 +59,36 @@ class PropertyGroup(Property):
 
     def __set__(self, instance, value):
         raise ValueError("You cannot set a PropertyGroup")
-        
+
+
 class RenderGlobals(PropertyGroup):
     threads = IntProperty()
     flags = StringProperty()
 
+
 # class Layer(PropertyGroup):
 #     objects = StringProperty()
 
-# class MainLayer(Layer):    
+# class MainLayer(Layer):
 class MainLayer(PropertyGroup):
     lights = StringProperty()
     objects = StringProperty()
-    
+
+
 # class OcclusionLayer(Layer):
-class OcclusionLayer(PropertyGroup):    
+class OcclusionLayer(PropertyGroup):
     material = StringProperty()
     objects = StringProperty()
+
 
 class LayerBlocks(PropertyGroup):
     main = MainLayer()
     occ = OcclusionLayer()
 
+
 class AppClass(object):
     __metaclass__ = PropertiesBased
-    
+
     def _printLeaf(self, parent, prop_path, prop):
         try:
             value = parent.__dict__[prop.name]
@@ -85,26 +96,29 @@ class AppClass(object):
             value = prop.default
         dotted_path = '.'.join(prop_path)
         print "{0} = {1}".format(dotted_path, value)
-    
+
     def _printGroup(self, prop_path, group):
         for prop in group.properties:
             prop_path.append(prop.name)
             if isinstance(prop, PropertyGroup):
-                self._printGroup(prop_path, group.__dict__[prop.name])
+                # self._printGroup(prop_path, group.__dict__[prop.name])
+                self._printGroup(prop_path, getattr(group, prop.name))
             else:
                 self._printLeaf(group, prop_path, prop)
             prop_path.pop()
-    
+
     def printFromRoot(self):
         prop_base_path = [self.__class__.__name__]
-        
+
         for prop in self.properties:
             prop_path = prop_base_path[:]
             prop_path.append(prop.name)
             if isinstance(prop, PropertyGroup):
-                self._printGroup(prop_path, self.__dict__[prop.name])
+                # self._printGroup(prop_path, self.__dict__[prop.name])
+                self._printGroup(prop_path, getattr(self, prop.name))
             else:
                 self._printLeaf(self, prop_path, prop)
+
 
 class RootNode(AppClass):
     frame_start = IntProperty()
